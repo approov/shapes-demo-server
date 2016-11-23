1564b59aa4f7f3cfe0c4b1f5aab6526f33baef8b
Artūrs Artamonovs <arthura@criticalblue.com>
2016-11-23 11:39:25 +0000

    Updated README. Replaced SECRET token value

--- runserver.py
+++ runserver.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 # Your Token Secret is generated for you when the Approov service is initialized
 # and it can be copied to your server code from your Approov Admin Portal
 # Set the secret as base64 encoded constant
-SECRET = "<YOUR TOKEN>"
+SECRET = bytes("+approov+secret+","utf-8")
 
 shapes=["Circle","Triangle","Square","Rectangle"]
 
@@ -36,7 +36,6 @@ def verifyToken(token, clientIP):
   # Get IP from token contents if present then check it
   try:
     issuedIP = (tokenContents['ip'])
-
     # Compare the issued IP with the requester IP
     if clientIP != issuedIP:
         # Requester IP did not match issued IP
@@ -50,7 +49,8 @@ def verifyToken(token, clientIP):
 
 @app.route("/")
 def returnShape():
-
+  print("---------------------------------------------------------------------")
+  
   # Get the Approov Token from header
   token = request.headers.get("ApproovToken")
 
@@ -60,6 +60,7 @@ def returnShape():
 
   # Get the requesters IP Address as IPv6 for consistency
   clientIP = request.remote_addr
+  print(clientIP)
   try:
     clientIPBin = socket.inet_pton(socket.AF_INET6, clientIP)
   except:
